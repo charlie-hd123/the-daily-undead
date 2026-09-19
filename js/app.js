231a6775cb2fabfdba64db9959d32e76a64b8106
charlie-hd123 <290023214+charlie-hd123@users.noreply.github.com>
2026-09-19 19:30:12 +0100

    Center account button labels

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ import {
   isValidDateKey,
   orderMapsForGame,
   toggleOrderedSelection,
-} from "./game-core.js?v=20260919-10";
+} from "./game-core.js?v=20260919-11";
 import {
   calculateReviveCost,
   canUseRequestedPreviewDate,
@@ -23,15 +23,15 @@ import {
   purchaseMissedDayRevive,
   resetReviveCount,
   shouldResetReviveCycle,
-} from "./progression.js?v=20260919-10";
-import { initialiseAccount } from "./account.js?v=20260919-10";
+} from "./progression.js?v=20260919-11";
+import { initialiseAccount } from "./account.js?v=20260919-11";
 import {
   fetchCommunityStats,
   formatCommunityCount,
   formatSolvePercentage,
   resolveCommunityStatsApiUrl,
   submitCommunityAttempt,
-} from "./community-stats.js?v=20260919-10";
+} from "./community-stats.js?v=20260919-11";
 
 const app = document.querySelector("#app");
 const dateLabel = document.querySelector("#puzzle-date");
--- js/marketing-core.js
+++ js/marketing-core.js
@@ -1,4 +1,4 @@
-import { getUtcDateKey, isValidDateKey } from "./game-core.js?v=20260919-10";
+import { getUtcDateKey, isValidDateKey } from "./game-core.js?v=20260919-11";
 
 export const firstRedditPostDate = "2026-08-13";
 
